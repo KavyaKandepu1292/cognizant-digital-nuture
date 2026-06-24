e6cf851e687912ce6b82982cc0103df94db42e63
KavyaKandepu1292 <23pa1a1292@vishnu.edu.in>
2026-06-24 11:45:58 +0530

    Delete FactoryMethodPatternExample directory

--- FactoryMethodPatternExample/Document.java
+++
@@ -1,7 +0,0 @@
-// Document.java
-// This is the BLUEPRINT — all document types must follow this
-
-public interface Document {
-    void open();    // every document must have open()
-    void close();   // every document must have close()
-}
